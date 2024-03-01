21a655047763ecb11fc973d1de5d8f489b2a8d78
Yashwanth <yashwathsaikumar@gmail.com>
2024-03-01 05:31:17 +0530

    firstDay

---
+++ create.sql
@@ -0,0 +1,6 @@
+create table cities(
+name varchar(50),
+country varchar(50),
+population INTEGER,
+area INTEGER
+);
---
+++ select.sql
@@ -0,0 +1,3 @@
+select * from cities;
+select name, country, population/area as density from cities;
+select name, country, population+area as add from cities;
